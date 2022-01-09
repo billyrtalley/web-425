bdaa2aef25adc91b6a42197396325b35b247da14
William Talley <wtalley@my365.bellevue.edu>
2022-01-08 22:38:59 -0600

    Exercise 1.5

--- week-1/profile-app/src/app/my-details/my-details.component.ts
+++ week-1/profile-app/src/app/my-details/my-details.component.ts
@@ -2,9 +2,9 @@
 Title: Exercise 1.5- Components
 Author: William Talley
  Date: 1/8/2022
-//Description: During this assignment, we created an Angular web application
-//The exercise was to create a new app, alter the html elements to fit the assignment requirements
-//and to run and test the application in the browser.
+Description: During this assignment, we created an Angular web application
+The exercise was to create a new app, alter the html elements to fit the assignment requirements
+and to run and test the application in the browser.
 
 Source Reference: Bellevue University web-425 GitHub repository
 Date Accessed: 6-8 January, 2022
