f2fef50a0567972d2e2366b5817ececf5c6b45d9
William Talley <wtalley@my365.bellevue.edu>
2022-01-08 22:31:41 -0600

    Exercise 1.5

--- week-1/profile-app/src/app/my-image/my-image.component.ts
+++ week-1/profile-app/src/app/my-image/my-image.component.ts
@@ -1,3 +1,21 @@
+/*
+Title: Exercise 1.5- Components
+Author: William Talley
+ Date: 1/8/2022
+//Description: During this assignment, we created an Angular web application
+//The exercise was to create a new app, alter the html elements to fit the assignment requirements
+//and to run and test the application in the browser.
+
+Source Reference: Bellevue University web-425 GitHub repository
+Date Accessed: 6-8 January, 2022
+Source URL: https://github.com/buwebdev/web-425
+Source Reference: Angular Tutorial for Beginners - Creating your first application | Lesson 2
+Author: Richard Krasso
+Date Accessed: 6-8 January, 2022
+Source URL: https://www.youtube.com/watch?v=XRm0SnUJROU&t=363s
+*/
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
