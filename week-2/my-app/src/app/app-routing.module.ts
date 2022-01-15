2351afb0995d4e7c68e794ad53df6c0150396633
William Talley <wtalley@my365.bellevue.edu>
2022-01-15 14:37:09 -0600

    exercise 2.2

---
+++ week-2/my-app/src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component'
+
+
+//go to the home component
+// http://localhost:4200
+// http://localhost:4200/home
+
+//goes to the contact component
+// http://localhost:4200/contact
+
+//goes to the about component
+// http://localhost:4200/about
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
