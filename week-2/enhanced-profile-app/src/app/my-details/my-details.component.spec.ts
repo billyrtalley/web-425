937be4bf27a3df33c0bde519ce54d1dba605c3b1
William Talley <wtalley@my365.bellevue.edu>
2022-01-15 20:31:49 -0600

    exercise 2.3 corrections

--- week-2/enhanced-profile-app/src/app/my-details/my-details.component.spec.ts
+++ week-2/enhanced-profile-app/src/app/my-details/my-details.component.spec.ts
@@ -1,3 +1,14 @@
+/*
+============================================
+; Title: Assignment 2.3 Data Binding
+; Author: Angular
+; Date: January 15, 2022
+; Modified By: William Talley
+; Description: This program demonstrates the
+;how to bind values to HTML elements in an Angular project.
+;===========================================
+*/
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MyDetailsComponent } from './my-details.component';
--- week-2/enhanced-profile-app/src/polyfills.ts
+++ week-2/enhanced-profile-app/src/polyfills.ts
@@ -51,3 +51,13 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+/*
+============================================
+; Title: Assignment 2.3 Data Binding
+; Author: Angular
+; Date: January 15, 2022
+; Modified By: William Talley
+; Description: This program demonstrates the
+;how to bind values to HTML elements in an Angular project.
+;===========================================
+*/
